5718200b7f981a3e531dcd8979a3d4543aaf9d25
franckbacquetwebforce <franckbacquet.formation@gmail.com>
2016-12-19 09:07:09 +0100

    lundi matin

--- app/Model/OrderModel.php
+++ app/Model/OrderModel.php
@@ -3,6 +3,7 @@
 namespace Model;
 
 use \W\Model\Model;
+use \Model\UsersModel;
 use \W\Model\ConnectionModel;
 
 
@@ -15,7 +16,31 @@ class OrderModel extends Model
   {
     $this->setTable('orders');
     $this->dbh = ConnectionModel::getDbh();
+    $this->usersmodel = new UsersModel();
+  }
+  /*
+  *findOrder
+  *récupère les commandes de l'utilisateur connecté
+  */
+  public function findOrder()
+  {
+    $user = $this->usersmodel->getLoggedUser();
 
+    $sql = "SELECT * FROM '.$this->table.' WHERE id_user = :id ORDER BY created_at DESC";
+      $sth = $this->dbh->prepare($sql);
+      $sth->bindValue(':id_user', $user['id']);
+      $sth->execute();
+    return $sth->fetchAll();
+  }
+  public function findOrderProducts($id)
+  {
+    $sql = "SELECT * FROM '.$this->table.
+
+    ' WHERE id_user = :id ORDER BY created_at DESC";
+      $sth = $this->dbh->prepare($sql);
+      $sth->bindValue(':id_user', $user['id']);
+      $sth->execute();
+    return $sth->fetchAll();
   }
 
 
