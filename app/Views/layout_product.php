6f9e3fcc8fe7307f9fccaaf2105102fec96b012b
Michele <mragot2@msn.com>
2016-12-21 15:10:01 +0100

    Michèle modif du 21/12 15h

--- app/Views/layout_product.php
+++ app/Views/layout_product.php
@@ -42,7 +42,7 @@
     					</ul>
     				</li>
     			</ul>
-    			<form class="navbar-form navbar-left">
+    			<form class="navbar-form navbar-left ">
     				<div class="form-group">
     					<input type="text" class="form-control" placeholder="Search">
     				</div>
